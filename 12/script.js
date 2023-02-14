60cbc79ccfa8805afb01bcf3ee88f795951e0a63
fatih5758 <hestuna@gmail.com>
2023-02-14 13:45:22 +0300

    12. ve 13. günler

---
+++ 12/script.js
@@ -0,0 +1,6 @@
+function yazi_duzenle(){
+    document.getElementById('demo').style.fontSize= '50px';
+    document.getElementById('demo').style.textAlign= 'center';
+    document.getElementById('demo').style.textDecoration= 'underline';
+    document.getElementById('demo').style.color= 'blue';
+}
